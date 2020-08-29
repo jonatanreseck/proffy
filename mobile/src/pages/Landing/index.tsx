f1eac76f9cc96594bb39343c3f002bbc086dd8c2
Jonatan <jonatanreseck@yahoo.com.br>
2020-08-29 11:54:40 -0300

    aula 4: criado os botões da landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
-import landingImg from '../../assets/images/landing.png';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
 
 function Landing(){
     return (
@@ -13,8 +18,23 @@ function Landing(){
                 Seja Bem Vindo, {'\n'}
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon}/>
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity> 
+
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon}/>
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </TouchableOpacity> 
+            </View>
+            <Text style={styles.totalConnection}>
+                Total de 285 conexões já realizadas {' '}
+                <Image source={heartIcon} />
+            </Text>
         </View>
     );
 }
-
+//52 minutes
 export default Landing;
--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -25,6 +25,45 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins_600SemiBold', 
     },
 
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between',
+    },
+
+    button: {
+        height:150,
+        width:'48%',
+        backgroundColor:'#333',
+        borderRadius:8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+
+    buttonPrimary: {
+        backgroundColor:'#9871f5',
+    },
+
+    buttonSecondary: {
+        backgroundColor:'#04d361',
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 20,
+    },
+
+    totalConnection: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#d4c2ff',
+        fontSize: 12,
+        lineHeight: 20,
+        maxWidth: 140,
+        marginTop: 40,
+    },
+
+
 });
 
 export default styles;
