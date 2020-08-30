80b4498576633969cadb3000df3c8f5af9b70019
Jonatan <jonatanreseck@yahoo.com.br>
2020-08-30 01:10:22 -0300

    aula 4: adic. navigations e pagina dar aulas

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -8,8 +10,14 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
-
 function Landing(){
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesPage(){
+        navigate('GiveClasses');
+    }
+
+
     return (
         <View style={styles.container} >
             <Image source={landingImg} style={styles.banner} />
@@ -19,15 +27,23 @@ function Landing(){
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, 
+                    styles.buttonPrimary]}
+                >
                     <Image source={studyIcon}/>
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity> 
+                </RectButton> 
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                <RectButton 
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, 
+                    styles.buttonSecondary]}
+                >
                     <Image source={giveClassesIcon}/>
                     <Text style={styles.buttonText}>Dar Aulas</Text>
-                </TouchableOpacity> 
+                </RectButton> 
             </View>
             <Text style={styles.totalConnection}>
                 Total de 285 conexões já realizadas {' '}
